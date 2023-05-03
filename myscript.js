c6e98189cea3ee99dfa7ee827c7d1f6e38ec2d81
shinriyo <shinriyo@gmail.com>
2023-05-03 12:01:48 +0900

    update 0.0.4

--- myscript.js
+++ myscript.js
@@ -4,24 +4,24 @@
 
   const fixer = function (elm) {
     const timestamp = elm.value;
-    const isMorning = timestamp.includes(' 午前');
-    const isNight = timestamp.includes(' 午後');
+    const isMorning = timestamp.includes(" 午前");
+    const isNight = timestamp.includes(" 午後");
     if (isMorning) {
-      const splited = timestamp.split(' ')[0];
+      const splited = timestamp.split(" ")[0];
       console.log(splited);
       elm.value = splited;
     } else if (isNight) {
-      const splited = timestamp.split(' ')[0];
-      const origHour = splited.split(':')[0];
+      const splited = timestamp.split(" ")[0];
+      const origHour = splited.split(":")[0];
       const hour = String(parseInt(origHour) + 12);
-      const replaced = splited.replace(`${origHour}:`, `${hour}:`)
+      const replaced = splited.replace(`${origHour}:`, `${hour}:`);
       console.log(splited);
       elm.value = replaced;
     }
-  }
+  };
 
   function fixTimeStamp() {
-    const elms = document.getElementsByClassName('add-timestamp-time');
+    const elms = document.getElementsByClassName("add-timestamp-time");
     if (elms && elms.length > 0) {
       const elm = elms[0];
       fixer(elm);
@@ -34,10 +34,10 @@
     function jsLoaded() {
       // 取得したい要素
       // console.log($('.container-text'));
-      if ($('.container-text') != null) {
+      if ($(".container-text") != null) {
         // clearInterval(jsInitCheckTimer);
         // テキストたち
-        var texts = $('.container-text');
+        var texts = $(".container-text");
         for (var i = 0; i < texts.length; i++) {
           texts[i].onclick = function () {
             var title = $(this).attr("title");
@@ -48,22 +48,22 @@
               // Toastをだす
               iqwerty.toast.toast(`${title}をコピーしました`);
             }, 1000);
-          }
+          };
         }
       }
     }
-  };
+  }
 
   window.addEventListener("load", copyClipboard, false);
 
   // 矢印キーからの
   function myFunction(num) {
-    var ids = document.getElementsByClassName('click-target-id');
+    var ids = document.getElementsByClassName("click-target-id");
 
     var no = 0;
     for (var i = 0; i < ids.length; i++) {
       var element = ids[i].parentElement.parentElement;
-      if (element.className.includes('selected')) {
+      if (element.className.includes("selected")) {
         no = i;
         break;
       }
@@ -83,21 +83,49 @@
 
   doc = document;
   readyState = doc.readyState;
-
-  if (readyState === 'complete' || readyState === 'interactive') {
+  if (readyState === "complete" || readyState === "interactive") {
     loadedListener();
+
+    // キー操作を無視させるため
+    var isOK = true;
+    function setTrue() {
+      isOK = true;
+    }
+    function setFalse() {
+      isOK = false;
+    }
+
     // 矢印キー
-    document.addEventListener('keydown', function (event) {
-      if (event.code == 'ArrowUp') {
-        // 上
-        myFunction(-1);
-      } else if (event.code == 'ArrowDown') {
-        // 下
-        myFunction(+1);
+    document.addEventListener("keydown", function (event) {
+      const textFields = document.querySelectorAll("input[type='text']");
+      for (var i = 0; i < textFields.length; i++) {
+        textFields[i].addEventListener("keydown", function () {
+          // 入力中
+          setFalse();
+        });
+        textFields[i].addEventListener("blur", function () {
+          // 入力終了
+          setTrue();
+        });
+      }
+
+      console.log("こら3");
+      if (isOK) {
+        switch (event.code) {
+          case "KeyK":
+          case "ArrowUp":
+            // 上
+            myFunction(-1);
+            break;
+          case "KeyJ":
+          case "ArrowDown":
+            // 下
+            myFunction(+1);
+            break;
+        }
       }
     });
   } else {
     doc.addEventListener("DOMContentLoaded", loadedListener, false);
   }
-})(function () {
-});
+})(function () {});
